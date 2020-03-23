618e2a822a5bcde76392a000c3ad14c62b5b740d
Whitney Griffith <whitney.griffith16@gmail.com>
2020-03-22 19:51:02 -0700

    init from scratch without create-react-app

---
+++ src/server/defaultState.js
@@ -0,0 +1,44 @@
+export const defaultState = {
+    users: [{
+        id: "U1",
+        name: "Dev"
+    }],
+    groups: [{
+        name: "To Do",
+        id: "G1",
+        owner: "U1"
+    },{
+        name: "Doing",
+        id: "G2",
+        owner: "U1"
+    },{
+        name: "Done",
+        id: "G3",
+        owner: "U1"
+    }],
+    tasks : [{
+        name: "Do tests",
+        id: "T1",
+        group: "G1",
+        owner: "U1",
+        isComplete: false
+    },{
+        name: "Doing tests",
+        id: "T2",
+        group: "G2",
+        owner: "U1",
+        isComplete: false
+    },{
+        name: "Tests done",
+        id: "T3",
+        group: "G3",
+        owner: "U1",
+        isComplete: true
+    }],
+    comments: [{
+        owner: "U1",
+        id: "C1",
+        task: "T1",
+        content: "Great work!!!"
+    }]
+}
