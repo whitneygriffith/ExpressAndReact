5337701b7beadba39c3472f716b766086ededba2
Whitney Griffith <whitney.griffith16@gmail.com>
2020-03-22 21:49:54 -0700

    added an action

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {requestTaskCreation} from '../store/mutations';
 
 export  const TaskList  =  ({tasks, name, id, createNewTask})  =>(
     <div>
@@ -31,6 +32,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         createNewTask(id){
             console.log("creating new tasks ", id);
+            dispatch(requestTaskCreation(id));
         }
     }
 };
--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import {defaultState} from '../../server/defaultState';
+import { createLogger } from 'redux-logger';
 
 export const store = createStore(
     function reducer(state = defaultState, action){
         return state; 
-    }
-)
+    },
+    applyMiddleware(createLogger())
+);
