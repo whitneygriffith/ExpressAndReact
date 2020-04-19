e636d44c3d471d2aa3999009ab194f5ca575e177
Whitney Griffith <whitney.griffith16@gmail.com>
2020-04-18 18:59:34 -0700

    route guard, session and authenticated added

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {  Provider } from 'react-redux';
-import {store} from '../store';
+import { store}  from '../store';
 import {ConnectedDashboard} from './Dashboard';
 import {ConnectedNavigation} from './Navigation';
 import { ConnectedTaskDetail  } from './TaskDetail';
 import { Router, Route } from 'react-router-dom';
 import { history } from '../store/history';
+import {Redirect} from 'react-router';
+
+const RouteGuard = Component =>({match})=>{
+    console.info("Route guard ", match);
+    if (!store.getState().session.authenticated){
+        return <Redirect to="/"/>
+    } {
+    return <Component match={match}/>
+    }
+}
 
 export const Main = () => (
     <Router history={history}>
         <Provider store={store}>
             <div>
                 < ConnectedNavigation />
-                <Route exact path="/dashboard" render={() => (<ConnectedDashboard/>)}/>
-                <Route exact path="/task/:id" render={({match}) => (<ConnectedTaskDetail  match={match}/>)}/>
+                <Route exact path="/dashboard" render={RouteGuard(ConnectedDashboard)}/>
+                <Route exact path="/task/:id" render={RouteGuard(ConnectedTaskDetail)}/>
             </div>
         </Provider>
     </Router>
--- src/app/store/index.js
+++ src/app/store/index.js
@@ -10,22 +10,25 @@ import { TaskList } from '../components/TaskList';
 
 export const store = createStore(
     combineReducers({
-         tasks(tasks = defaultState.tasks, action){
-             switch(action.type){
-                 case mutations.CREATE_TASK:
-                     return [ ...tasks, {
-                         id: action.taskID,
-                         name: "New Task",
-                         group: action.groupID,
-                         owner: action.ownerID,
-                         isComplete: false
-                     }]
-                 case mutations.SET_TASK_COMPLETE:
-                     return tasks.map(task =>  {
-                        return (task.id === action.taskID) ? 
-                            { ...task, isComplete: action.isComplete} : 
-                            task;
-                     });  
+        session(session = defaultState.session){
+            return session;
+        },
+        tasks(tasks = defaultState.tasks, action){
+            switch(action.type){
+                case mutations.CREATE_TASK:
+                    return [ ...tasks, {
+                        id: action.taskID,
+                        name: "New Task",
+                        group: action.groupID,
+                        owner: action.ownerID,
+                        isComplete: false
+                    }]
+                case mutations.SET_TASK_COMPLETE:
+                    return tasks.map(task =>  {
+                    return (task.id === action.taskID) ? 
+                        { ...task, isComplete: action.isComplete} : 
+                        task;
+                    });  
                 case mutations.SET_TASK_NAME:
                     return tasks.map(task =>  {
                         return (task.id === action.taskID) ? 
