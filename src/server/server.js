c28b19ff78c9154a2ca5371ffe6a6bde0ac0582c
Whitney Griffith <whitney.griffith16@gmail.com>
2020-04-18 22:05:43 -0700

    auth implemented in  server

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import {connectDB} from './connect-db';
 import './initialize-db';
+import { authenticationRoute } from './authenticate';
 
 let port = 7777;
 let app = express();
@@ -15,6 +16,8 @@ app.use(
     bodyParser.urlencoded({extended:true}),
     bodyParser.json()
 );
+
+authenticationRoute(app);
 
 export const addNewTask = async task=>{
     let db = await connectDB();
